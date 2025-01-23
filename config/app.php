0d8e9fddefc7c07c7f7d31341ed988f962bbdd48
Juan Lago <juan@leadsupply.com>
2025-01-23 14:01:35 +0100

    Registered FilesystemServiceProvider because it's not loaded by tests.

--- config/app.php
+++ config/app.php
@@ -55,6 +55,7 @@ return [
 
     'providers' => [
         App\Providers\AppServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
         Illuminate\Notifications\NotificationServiceProvider::class,
         Illuminate\Notifications\SlackChannelServiceProvider::class,
         Spatie\GoogleCloudStorage\GoogleCloudStorageServiceProvider::class,
